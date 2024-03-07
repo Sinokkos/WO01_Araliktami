43e441177615edc7c6ac0761eda30da8e0106171
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-07 14:22:49 +0300

    some changes

--- WO04_SifirGireneKadarToplamVeOrtalama/Program.cs
+++ WO04_SifirGireneKadarToplamVeOrtalama/Program.cs
@@ -6,8 +6,30 @@
         {
             // Klavyeden 0 değeri girinceye kadar girilen sayıların toplamını ve ortalamasını bulan pp
 
+            int sayi = 0, toplam = 0, adet=0;
+
+            Console.WriteLine("Lütfen Sayı giriniz  :  ");
+            sayi =Convert.ToInt32(Console.ReadLine());
+            toplam = sayi;
+
+
+            while (sayi !=0)
+            {
+                Console.WriteLine("Lütfen sayı giriniz :  ");
+                sayi= Convert.ToInt32(Console.ReadLine());
+
+                toplam += sayi;
+                if (sayi != 0) 
+                {
+                    adet++;
+                } 
+            }
+
+            Console.WriteLine($"\n\nToplam = {toplam}, Ortalama = {toplam/adet}");
+
 
             Console.ReadKey();
+
         }
     }
 }
