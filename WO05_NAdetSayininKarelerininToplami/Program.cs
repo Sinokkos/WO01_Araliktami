ddfeb776d43496c5aead544b237da3e7c10651a7
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-07 14:42:49 +0300

    some changes

--- WO05_NAdetSayininKarelerininToplami/Program.cs
+++ WO05_NAdetSayininKarelerininToplami/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Klavyeden okunacak N adet sayının karelerinin toplamı
+            // for ile yapılabilir
+
+            int adet = 0, sayi=0;
+            double toplam = 0;
+
+            Console.Write("Kaç adet sayı gireceksin?    ");
+            adet=Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i <= adet; i++)
+            {
+                Console.Write($"{i}.sayıyı giriniz:  ");
+                sayi=Convert.ToInt32(Console.ReadLine());
+
+                toplam += Math.Pow(sayi, 2);
+            }
+
+            Console.WriteLine($"Girilen sayıların karelerinin/küp toplamı : {toplam}");
+
+
+            Console.ReadKey ();
         }
     }
 }
