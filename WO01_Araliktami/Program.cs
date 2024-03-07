2a805958003b6acf95eaa1eceab6e7a7e66c70a4
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-07 12:33:12 +0300

    Add project files.

---
+++ WO01_Araliktami/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Design;
+
+namespace WO01_Araliktami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+             // Ekrandan girilen bir sayının 0-100 arasında olup olmadığını gösteren (arasında veya arasında değil şeklinde gösteren pp
+
+            int sayi = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Lütfen Sayınızı Giriniz :");
+                sayi = Convert.ToInt32(Console.ReadLine());
+
+
+                if (sayi >= 0 && sayi <= 100)
+                {
+                    Console.WriteLine($"Girmiş olduğunuz sayı: {sayi} 0-100 arasındadır..");
+                }
+                else
+                {
+                    Console.WriteLine($"Girmiş olduğunuz sayı: {sayi} 0-100 arasında değildir...");
+                }
+
+            } while (true);
+            
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
